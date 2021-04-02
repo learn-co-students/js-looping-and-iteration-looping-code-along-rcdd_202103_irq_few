c927cab87696c684440bdc0db61352a28d88967e
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-02 20:27:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(array, event){
+  const thankYouMessage = [];
+  let thankYou ;
+  for (let i = 0; i < array.length; i++){
+       thankYou = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+      thankYouMessage.push(thankYou);
+  }
+  return thankYouMessage;
+  
+}
+
+
+function countDown(countDown){
+  
+  while(countDown >= 0){
+    console.log(countDown);
+    countDown--;
+  }
+  
+}
+
+
+writeCards(["Lisa", "Kaitlin", "Jan"], 'surprise');
+countDown(10);
