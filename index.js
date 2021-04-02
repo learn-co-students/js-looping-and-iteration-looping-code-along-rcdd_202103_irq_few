ffead822cb5d847356de7b5512b2c517be23e084
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-02 21:38:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(cards, event) {
+  let eventName;
+  let finalEvent = [];
+  for(let i=0; i<cards.length; i++){
+    eventName = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
+    finalEvent.push(eventName);
+  }
+  return finalEvent;
+}
+writeCards(["Lisa", "Kaitlin", "Jan"],"surprise");
+
+function countDown (num) {
+  let i = num;
+  while (i>=0){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
