313953dd189700f0a359e0ee49d316ebe0fb6406
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-03 11:27:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push('Thank you, ' + names[i] +', for the wonderful '+eventName+' gift!');
+  }
+  return result;
+}
+
+function countDown(boundery){
+  while (boundery >= 0) {
+    console.log(boundery);
+    boundery--;
+  }
+}
