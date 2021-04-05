38a984a4f58b56dfdbf2af34e5b06296a15f6ccd
Hasar Ali <hasar.ali@protonmail.com>
2021-04-05 12:49:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
+
+function writeCards(names, event) {
+  return names.map((name) => {
+    return `Thank you, ${name}, for the wonderful ${event} gift!`;
+  })
+}
