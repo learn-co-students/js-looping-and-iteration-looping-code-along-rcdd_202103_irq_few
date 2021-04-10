c2a7e1c6e58afd9dea4d2ec4c2860d11add1b968
Omer Sabah <omerrj99@gmail.com>
2021-04-10 11:38:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const countDown=(n)=>{
+  while (n>=0){
+    console.log(n);
+    n--;
+  }
+}
+
+countDown(10);
+
+const writeCards=(names,event)=>{
+  
+  for(let i=0;i<names.length;i++){
+    names[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+  
+  return names;
+}
