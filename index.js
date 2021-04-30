e5932729727299154f3106175891dd4dab6f2d0f
Yusra Ahmed <isra.00287446@gmail.com>
2021-04-30 22:36:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array,name){
+  let arr=[];
+  for (let i=0;i<array.length;i++){
+    arr.push( "Thank you, "+array[i]+", for the wonderful "+name+" gift!");
+  }
+  return arr;
+  
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(num){
+  let i=0;
+  while(i<=num){
+   console.log(num);
+   num--;
+  }
+}
+countDown(10);
