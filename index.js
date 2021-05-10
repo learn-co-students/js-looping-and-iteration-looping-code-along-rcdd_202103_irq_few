7d07bfda944fd8aaac43449578f6386d89c5a91d
Mariam  Khamo <mariamw736@gmail.com>
2021-05-10 09:55:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names,events) {
+    let messageArray=[];
+    for (let i = 0; i <names.length; i++) {
+      messageArray.push('Thank you, '+names[i]+', for the wonderful ' + events+' gift!');
+    }
+    return messageArray;
+  }
+  writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
+function countDown(numb){
+let i=numb;
+while (i>=0){
+    console.log(i--);
+}
+}
+countDown(10);
